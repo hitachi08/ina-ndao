232f838a5184efcc8bb0bdf0f087658598c8e236
mirayana <herijuart01@gmail.com>
2025-09-27 23:10:22 +0800

    tambahan bagian galeri

--- galeri.php
+++ galeri.php
@@ -68,103 +68,27 @@
     </div>
     <!-- Hero End -->
 
-
-    <!-- Project Start -->
-    <div class="container-fluid py-5">
-        <div class="container py-5">
-            <div class="row g-0">
-                <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
-                    <div class="d-flex flex-column justify-content-center bg-primary h-100 p-5">
-                        <h1 class="text-white mb-5">Our Latest <span
-                                class="text-uppercase text-primary bg-light px-2">Projects</span></h1>
-                        <h4 class="text-white mb-0"><span class="display-1">6</span> of our latest projects</h4>
-                    </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="row g-0">
-                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.2s">
-                            <div class="project-item position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="img/project-1.jpg" alt="">
-                                <a class="project-overlay text-decoration-none" href="#!">
-                                    <h4 class="text-white">Kitchen</h4>
-                                    <small class="text-white">72 Projects</small>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.3s">
-                            <div class="project-item position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="img/project-2.jpg" alt="">
-                                <a class="project-overlay text-decoration-none" href="#!">
-                                    <h4 class="text-white">Bathroom</h4>
-                                    <small class="text-white">67 Projects</small>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.4s">
-                            <div class="project-item position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="img/project-3.jpg" alt="">
-                                <a class="project-overlay text-decoration-none" href="#!">
-                                    <h4 class="text-white">Bedroom</h4>
-                                    <small class="text-white">53 Projects</small>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
-                            <div class="project-item position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="img/project-4.jpg" alt="">
-                                <a class="project-overlay text-decoration-none" href="#!">
-                                    <h4 class="text-white">Living Room</h4>
-                                    <small class="text-white">33 Projects</small>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.6s">
-                            <div class="project-item position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="img/project-5.jpg" alt="">
-                                <a class="project-overlay text-decoration-none" href="#!">
-                                    <h4 class="text-white">Furniture</h4>
-                                    <small class="text-white">87 Projects</small>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.7s">
-                            <div class="project-item position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="img/project-6.jpg" alt="">
-                                <a class="project-overlay text-decoration-none" href="#!">
-                                    <h4 class="text-white">Rennovation</h4>
-                                    <small class="text-white">69 Projects</small>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Project End -->
-
-
     <!-- Testimonial Start -->
-    <div class="container-xxl pb-5">
-        <div class="container pb-5">
+    <div class="container-fluid py-5">
+        <div class="container-xxl pb-5">
+            <h1 class="mb-5 text-center">Galeri Produk <span class="text-uppercase text-primary">Ina Ndao</span>
+            </h1>
             <div class="row justify-content-center">
-                <div class="col-md-12 col-lg-9">
+                <div class="col-md-10 col-lg-9">
                     <div class="owl-carousel testimonial-carousel wow fadeIn" data-wow-delay="0.2s">
                         <div class="testimonial-item">
                             <div class="row g-5 align-items-center">
                                 <div class="col-md-6">
                                     <div class="testimonial-img">
-                                        <img class="img-fluid" src="img/testimonial-1.jpg" alt="">
+                                        <img class="img-fluid" src="img\produk\sarung-alor.png" alt="">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="testimonial-text pb-5 pb-md-0">
-                                        <h3>Sustainable Material</h3>
-                                        <p>Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit, sed
-                                            stet no labore lorem sit. Sanctus clita duo justo et tempor eirmod magna
-                                            dolore erat
-                                            amet</p>
-                                        <h5 class="mb-0">Boris Johnson</h5>
+                                        <h3>Kain Tenun Alor</h3>
+                                        <p>Motif ikan melambangkan sumber penghidupan utama bagi masyarakat Alor
+                                            yang sebagian besar bergantung pada hasil laut.</p>
+                                        <h5 class="mb-0">Motif Ikan</h5>
                                     </div>
                                 </div>
                             </div>
@@ -213,11 +137,187 @@
         </div>
     </div>
     <!-- Testimonial End -->
+    <div class="container py-5">
+        <div class="row g-4">
+
+            <!-- Item 1 -->
+            <div class="col-lg-6 col-md-6 wow fadeIn" data-wow-delay="0.1s">
+                <div class="row g-3 align-items-center">
+                    <!-- Gambar -->
+                    <div class="col-5">
+                        <div class="team-item position-relative overflow-hidden shadow">
+                            <img class="img-fluid w-100" src="img/produk/sarung-sumba.png" alt="Kain Tenun Ina Ndao">
+                            <div class="team-overlay">
+                                <small class="mb-2">Motif Sumba</small>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Narasi -->
+                    <div class="col-7">
+                        <h5 class="mb-2">Motif Sumba</h5>
+                        <p class="small" style="text-align:justify;">
+                            Tenun Ikat Ina Ndao berasal dari Kupang, NTT. Setiap lembar kain memiliki makna
+                            filosofi yang diwariskan dari generasi ke generasi.
+                        </p>
+                        <!-- Tombol Modal -->
+                        <a href="#!" class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#modalSumba">Lebih Lanjut</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Item 2 -->
+            <div class="col-lg-6 col-md-6 wow fadeIn" data-wow-delay="0.3s">
+                <div class="row g-3 align-items-center">
+                    <div class="col-5">
+                        <div class="team-item position-relative overflow-hidden shadow">
+                            <img class="img-fluid w-100" src="img/produk/sarung-ende.png" alt="Kain Tenun Ina Ndao">
+                            <div class="team-overlay">
+                                <small class="mb-2">Motif Ende</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-7">
+                        <h5 class="mb-2">Motif Ende</h5>
+                        <p class="small" style="text-align:justify;">
+                            Proses pewarnaan kain menggunakan bahan alami, ramah lingkungan,
+                            dan menjaga keaslian tradisi tenun ikat.
+                        </p>
+                        <!-- Tombol Modal -->
+                        <a href="#!" class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#modalEnde">Lebih Lanjut</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Modal untuk Motif Sumba -->
+    <div class="modal fade" id="modalSumba" tabindex="-1" aria-labelledby="modalSumbaLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalSumbaLabel">Cerita Lengkap Motif Sumba</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
+                </div>
+                <div class="modal-body text-center">
+                    <!-- Gambar di tengah -->
+                    <img src="img/produk/sarung-sumba.png" class="img-fluid mx-auto d-block mb-3" alt="Motif Sumba"
+                        style="max-height:350px; object-fit:contain;">
+
+                    <p style="text-align:justify;">
+                        Motif Sumba memiliki nilai filosofi yang sangat dalam, menggambarkan hubungan manusia
+                        dengan alam dan leluhur. Setiap pola yang ditenun memiliki makna simbolis tentang
+                        kehidupan, ketekunan, dan kepercayaan masyarakat Sumba. Proses pembuatannya dilakukan
+                        dengan penuh ketelitian, mulai dari pemintalan benang, pewarnaan alami, hingga teknik
+                        ikat yang khas.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal untuk Motif Ende -->
+    <div class="modal fade" id="modalEnde" tabindex="-1" aria-labelledby="modalEndeLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalEndeLabel">Cerita Lengkap Motif Ende</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
+                </div>
+                <div class="modal-body text-center">
+                    <!-- Gambar di tengah -->
+                    <img src="img/produk/sarung-ende.png" class="img-fluid mx-auto d-block mb-3" alt="Motif Ende"
+                        style="max-height:350px; object-fit:contain;">
+
+                    <p style="text-align:justify;">
+                        Motif Ende dikenal dengan warnanya yang khas dan proses pewarnaan alami dari bahan-bahan
+                        tumbuhan sekitar. Warna-warna ini melambangkan keselarasan hidup, harapan, dan semangat
+                        masyarakat Ende dalam menjaga tradisi. Teknik ikatnya yang rumit membuat setiap kain
+                        menjadi unik dan bernilai tinggi.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Project Start -->
+    <div class="container py-5">
+        <div class="row g-0">
+            <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
+                <div class="d-flex flex-column justify-content-center bg-primary h-100 p-5">
+                    <h1 class="text-white mb-5">Our Latest <span
+                            class="text-uppercase text-primary bg-light px-2">Projects</span></h1>
+                    <h4 class="text-white mb-0"><span class="display-1">6</span> of our latest projects</h4>
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="row g-0">
+                    <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.2s">
+                        <div class="project-item position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="img/project-1.jpg" alt="">
+                            <a class="project-overlay text-decoration-none" href="#!">
+                                <h4 class="text-white">Kitchen</h4>
+                                <small class="text-white">72 Projects</small>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.3s">
+                        <div class="project-item position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="img/project-2.jpg" alt="">
+                            <a class="project-overlay text-decoration-none" href="#!">
+                                <h4 class="text-white">Bathroom</h4>
+                                <small class="text-white">67 Projects</small>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.4s">
+                        <div class="project-item position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="img/project-3.jpg" alt="">
+                            <a class="project-overlay text-decoration-none" href="#!">
+                                <h4 class="text-white">Bedroom</h4>
+                                <small class="text-white">53 Projects</small>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                        <div class="project-item position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="img/project-4.jpg" alt="">
+                            <a class="project-overlay text-decoration-none" href="#!">
+                                <h4 class="text-white">Living Room</h4>
+                                <small class="text-white">33 Projects</small>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.6s">
+                        <div class="project-item position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="img/project-5.jpg" alt="">
+                            <a class="project-overlay text-decoration-none" href="#!">
+                                <h4 class="text-white">Furniture</h4>
+                                <small class="text-white">87 Projects</small>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.7s">
+                        <div class="project-item position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="img/project-6.jpg" alt="">
+                            <a class="project-overlay text-decoration-none" href="#!">
+                                <h4 class="text-white">Rennovation</h4>
+                                <small class="text-white">69 Projects</small>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Project End -->
 
     <!-- Footer Start -->
     <?php include "footer.html" ?>
     <!-- Footer End -->
-     
+
     <!-- Back to Top -->
     <a href="#!" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
 
